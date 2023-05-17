72744cf9036c157c3ccf8abffca4e94568c0a5f1
SwaminathanViswa <sviswa2002@gmail.com>
2023-05-17 12:04:29 +0800

    Learnt about js

--- my-js-file.js
+++ my-js-file.js
@@ -22,8 +22,8 @@ output --> fail
 var b = 0;
 var c = 2;
 
-b = a--; //postfix (so basically, b=a is applied first and then the value of a is incremented)
-c = --a; //prefix (for this case, value of a is incremented first and then the value of a is assigned to c)
+b = a++; //postfix (so basically, b=a is applied first and then the value of a is incremented)
+c = ++a; //prefix (for this case, value of a is incremented first and then the value of a is assigned to c)
 
 console.log({a,b,c});*/
 
@@ -135,3 +135,82 @@ function printNum() {
   }
 }
 printNum();*/
+
+//maps (like for loop)
+
+/* // the letter i just represent each element, can change it with any letter
+myArray = [1,2,3]
+const newArray = myArray.map((i) => (i+1));
+console.log(newArray);
+console.log(myArray);*/
+
+//filter
+
+/*myArray = [10,20,50];
+newArray = myArray.filter(e => e<50);
+console.log(newArray);
+console.log(myArray);*/
+
+// ES 6
+// Default parameter
+
+// the name = " world" is the default parameter if there is no input given
+/*function hello(name="world") {
+  console.log("hello " +name);
+}
+hello("viswa");*/
+
+// Template Literals
+/*console.log(`
+  Apple
+  Banana
+  Mango
+  grapes  `
+);*/
+
+
+//Scoping
+
+/*function outer() {
+  var name1 = "xyz";
+  let name2 = "pqr";
+
+// The inner function only has a scope from lines 179 to 182.
+//Hence it doesnt change the name1 from "xxyyzz" to "xyz"
+  function inner() {
+    var name1 = "xxyyzz";
+    console.log(name1);
+  }
+  inner();
+  console.log(name1);
+}
+outer();*/
+
+//ASYNCHOROUS JS (it just means that js will get the data but not in the real time, will give priority to the synchronous data first)
+
+//PROMISES
+//for fetching data, we will use the keyword "fetch" and put the url inside (e.g. shown below)
+
+/*//First we are fetching the data from the website
+fetch("https://jsonplaceholder.typicode.com/users")
+//then we are changing the format of the data into a json file (json just converts any form of received data into key:value pairs)
+.then((result) => result.json())
+//then we store it in data and we log it to the console
+.then((data) => console.log(data));*/
+
+
+//we can write in a neater and in a await format
+
+/*const getData = async () => {
+  const result = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await result.json();
+  console.log(data);
+};
+getData();*/
+
+/*async function getData() {
+  const result = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await result.json();
+  console.log(data);
+}
+getData();*/
